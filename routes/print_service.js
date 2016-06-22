1bbd3f9facb11925d7ce2b05d4812d4d89327162
Md Mahfuzur Rahman Siddiquee <mahfuz.discover@gmail.com>
2016-06-22 14:46:38 +0200

    ssl errors neglected

--- routes/print_service.js
+++ routes/print_service.js
@@ -2,16 +2,17 @@ var config = require('../config');
 var phantom = require('phantom');
 
 var print_with_phantom = function(req, content_url, file_name, callback) {
-	phantom.create().then(function(ph) {
+	phantom.create(['--ignore-ssl-errors=yes']).then(function(ph) {
 	    ph.createPage().then(function(page) {
 			
 			page.setContent(req.body.html, content_url);
-
+			// console.log(req.body.html);
 			page.property('viewportSize', req.body.viewportSize).then(function() {
 				page.property('paperSize', req.body.paperSize).then(function() {
 
 					setTimeout(function() {
 			        	page.render(__dirname + '/../public/' + file_name);
+			        	ph.exit();
 			        	callback(file_name);
 			        }, 1000);
 				});
